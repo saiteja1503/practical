32bd0ee08867a831eb7ebd1a92100ecbe59ccf77
saiteja1503 <2420090114@klh.edu.in>
2025-07-26 10:45:47 +0530

    Add files via upload

---
+++ Arithmeticoperators.java
@@ -0,0 +1,19 @@
+package oppspractical;
+import java.util.Scanner;
+
+public class Arithmeticoperators {
+			  public static void main(String[] args) {
+			    
+			    int a = 12, b = 5;
+
+			    System.out.println("a + b = " + (a + b));
+
+			    System.out.println("a - b = " + (a - b));
+
+			    System.out.println("a * b = " + (a * b));
+
+			    System.out.println("a / b = " + (a / b));
+
+			    System.out.println("a % b = " + (a % b));
+			  }
+			}
---
+++ incrementdecrementoperations.java
@@ -0,0 +1,24 @@
+package oppspractical;
+import java.util.Scanner;
+
+public class incrementdecrementoperations {
+		  public static void main(String[] args) {
+		    
+		    int a = 12, b = 12;
+		    int result1, result2;
+
+		    // original value
+		    System.out.println("Value of a: " + a);
+
+		    result1 = ++a;
+		    System.out.println("After increment: " + result1);
+
+		    System.out.println("Value of b: " + b);
+
+		    result2 = --b;
+		    System.out.println("After decrement: " + result2);
+		  }
+		}
+
+
+
